69e3fc30482a1780a4a6671215672a86cba53987
anil pdv <pdvanil007@gmail.com>
2020-11-06 18:52:22 +0530

    fixing social media link

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: `A Simple blog with lot of information.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `anilpdv42`,
+      twitter: `anil_pdv`,
     },
   },
   plugins: [
